133e56ce38c4e8de0a80659a2e100786b57f83e1
jingray <lionel_jing@163.com>
2017-10-21 20:10:11 +0800

    robot localization and entropy

---
+++ RobotLocalization.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-10-21 11:40:37
+# @Author  : jingray (lionel_jing@163.com)
+# @Link    : http://www.jianshu.com/u/01fb0364467d
+# @Version : $Id$
+
+import os
+
+# Entropy=Σ(−p×log(p))
+# In general, entropy represents the amount of uncertainty in a system.
+# Since the measurement update step decreases uncertainty, entropy will decrease.
+# The movement step increases uncertainty, so entropy will increase after this step.
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'red']
+motions = [1,1]
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]   # -1%5 = 4
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
+
+for k in range(len(measurements)):
+    p = sense(p, measurements[k])
+    p = move(p, motions[k])
+
+print p
