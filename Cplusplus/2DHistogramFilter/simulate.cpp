d740d09562d213f5ca5f24aa18923f9e72665b31
jingray <lionel_jing@163.com>
2017-11-12 20:34:48 +0800

    complete simulate.cpp

--- Cplusplus/2DHistogramFilter/simulate.cpp
+++ Cplusplus/2DHistogramFilter/simulate.cpp
@@ -11,6 +11,7 @@
 
 #include "localizer.cpp"
 #include <algorithm>
+#include <time.h>
 // #include "helpers.cpp"
 
 class Simulation {
@@ -23,6 +24,8 @@ private:
 		for (i=0; i<height; i++) {
 			for (j=0; j<width; j++) {
 				color = grid[i][j];
+				/*InputIt find( InputIt first, InputIt last, const T& value ); 
+				find value from[first, last),if no value in [first, last), return last*/
 				if(std::find(all_colors.begin(), all_colors.end(), color) != all_colors.end()) {
 				    /* v contains x */
 				} else {
@@ -35,6 +38,44 @@ private:
 		colors = all_colors;
 		num_colors = colors.size();
 		return colors;
+	}
+
+	char get_observed_color()
+	{
+
+		int y = true_pose[0];
+		int x = true_pose[1];
+
+        char true_color = grid[y][x];
+        char color;
+        vector<char> possible_colors;
+        srand(time(0));
+        if (((float)rand() / RAND_MAX) < incorrect_sense_prob)
+        {
+        	colors = get_colors();
+        	for (int i = 0; i < colors.size(); ++i)
+        	{
+        		color = colors[i];
+
+	        	if(std::find(possible_colors.begin(), possible_colors.end(), color) != possible_colors.end()) 
+	        	{
+					/* v contains x */
+				} 
+				else if(color != true_color)
+				{
+					possible_colors.push_back(color);
+					cout << "adding color " << color << endl;
+				    /* v does not contain x */
+				}
+			}
+			srand(unsigned(time(0)));
+            color = possible_colors[rand()%possible_colors.size()];
+        }
+        else
+        {
+        	color = true_color;
+        }
+        return color;
 	}
 
 public: 
@@ -50,6 +91,8 @@ public:
 
 	vector <char> colors;
 	Simulation(vector < vector<char> >, float, float, vector <int>);
+	void Sense();
+	void Move(int, int);
 
 };
 
@@ -70,8 +113,25 @@ Simulation::Simulation(vector < vector <char> > map,
 	incorrect_sense_prob = p_miss / (p_hit + p_miss);
 	true_pose = start_pos;
 	prev_pose = true_pose;
+	height = map.size();
+	width = map[0].size();
 }
 
+void Simulation::Sense()
+{
+	char color = get_observed_color();
+	beliefs = sense(color,grid,beliefs,p_hit,p_miss);
+}
+
+void Simulation::Move(int dy, int dx)
+{
+	int new_y = (true_pose[0] + dy + height) % height;
+	int new_x = (true_pose[1] + dx + width) % width;
+	prev_pose = true_pose;
+	true_pose[0] = new_y;
+	true_pose[1] = new_x;
+	beliefs = move(dy, dx, beliefs, blur);
+}
 /**
 You can test your code by running this function. 
 
@@ -107,7 +167,11 @@ running the output.
 // 	// simulation = Simulation(map, 0.1, 0.9, pose);
 // 	cout << "initialization success!\n";
 // 	show_grid(map);
-
+// 	show_grid(simulation.beliefs);
 // 	cout << "x, y = (" << simulation.true_pose[0] << ", " << simulation.true_pose[1] << ")" << endl;
+// 	simulation.Sense();
+// 	show_grid(simulation.beliefs);
+// 	simulation.Move(1,1);
+// 	show_grid(simulation.beliefs);
 // 	return 0;
 // }
