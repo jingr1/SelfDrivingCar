c1284c5337b3c6e0dc0bcb158e34d727b6628d49
jingray <lionel_jing@163.com>
2017-11-18 21:02:25 +0800

    optimize normalize

--- Cplusplus/optimized_code/normalize.cpp
+++ Cplusplus/optimized_code/normalize.cpp
@@ -2,37 +2,25 @@
 using namespace std;
 
 // OPTIMIZATION: Pass variable by reference
-vector< vector<float> > normalize(vector< vector <float> > grid) {
+vector< vector<float> > normalize(vector< vector <float> > &grid) {
 
   	// OPTIMIZATION: Avoid declaring and defining 				// intermediate variables that are not needed.
 	float total = 0.0;
 	int i;
 	int j;
-	vector<float> row;
-	vector<float> newRow;
-	float oldProb;
 	for (i = 0; i < grid.size(); i++)
 	{
-		row = grid[i];
-		for (j=0; j< row.size(); j++)
+		for (j=0; j< grid[0].size(); j++)
 		{
-			oldProb = row[j];
-			total += oldProb;
+			total += grid[i][j];
 		}
 	}
-
-	vector< vector<float> > newGrid;
 
 	for (i = 0; i < grid.size(); i++) {
-		vector<float> row = grid[i];
-		newRow.clear();
-		for (j=0; j< row.size(); j++) {
-			float oldProb = row[j];
-			float newProb = oldProb / total;
-			newRow.push_back(newProb);
+		for (j=0; j< grid[0].size(); j++) {
+			grid[i][j] = grid[i][j]/total;
 		}
-		newGrid.push_back(newRow);
 	}
 
-	return newGrid;
+	return grid;
 }
