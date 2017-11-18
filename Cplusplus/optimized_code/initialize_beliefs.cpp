ee4258b93addcd6c5aaef209360c9c3ae3373d64
jingray <lionel_jing@163.com>
2017-11-18 20:23:16 +0800

    optimize initialize_beliefs

--- Cplusplus/optimized_code/initialize_beliefs.cpp
+++ Cplusplus/optimized_code/initialize_beliefs.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 // OPTIMIZATION: pass large variables by reference
-vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
+vector< vector <float> > initialize_beliefs(vector< vector <char> > &grid) {
 	// initialize variables for new grid
 	// OPTIMIZATION: Which of these variables are necessary?
 	// OPTIMIZATION: Reserve space in memory for vectors
+    int height = grid.size();
+	int width = grid[0].size();
+	int i, j;
+	float prob_per_cell;
   	vector< vector <float> > newGrid;
+  	newGrid.reserve(height);
 	vector<float> newRow;
-	int i, j, height, width, area;
-	float total, prob_per_cell;
-
-	height = grid.size();
-	width = grid[0].size();
- 
-	area = height * width;
-
+	newRow.reserve(width);
 	// calculate initial grid values
-	prob_per_cell = 1.0 / ( (float) area) ;
+	prob_per_cell = 1.0 / ( (float) (height * width)) ;
 	// store initial values in a new 2D grid with same size as grid
   	// OPTIMIZATION: Is there a way to get the same results 	// without nested for loops?
-	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
-		for (j=0; j<grid[0].size(); j++) {
-			newRow.push_back(prob_per_cell);
-		}
+
+	for (j=0; j<width; j++) 
+	{
+		newRow.push_back(prob_per_cell);
+	}
+	for (i=0; i<height; i++) 
+	{
 		newGrid.push_back(newRow);
 	}
 	return newGrid;
